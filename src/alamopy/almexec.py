d224c048732ce4f3f60d0ea990c6c2806c79eca7
Ilya Orson Sandoval <ilya.orson@gmail.com>
2022-12-05 17:19:25 +0000

    use abstract base location

--- src/alamopy/almexec.py
+++ src/alamopy/almexec.py
@@ -19,10 +19,13 @@ import subprocess
 import almutils as almutils
 from pathlib import Path
 
-# base_path = Path("/home/io")
-base_path = Path("/content/")
+base_path = Path(__file__).resolve().parent.parent.parent.parent
+# base_path = Path("/home/io")  # wsl
+# base_path = Path("/content/")  # collab
 
 exec_path = base_path / "alamo-linux64" / "alamo"
+
+print(f"Expected ALAMO executable location: {exec_path}")
 
 def exec_alamo(opts):
     """
