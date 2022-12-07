de36d3faa6f05f672d44ae77087817d6479e1365
Ilya Orson Sandoval <ilya.orson@gmail.com>
2022-12-07 19:42:58 +0000

    always update the master tagged docker

--- build_upload_docker.sh
+++ build_upload_docker.sh
@@ -3,4 +3,6 @@ LAST_COMMIT=$(git rev-parse --short HEAD)
 DOCKER_TAG=ilyaorson/symbolic_regression:$LAST_COMMIT
 docker build --no-cache -t $DOCKER_TAG -f Dockerfile_from_scratch .
 docker push $DOCKER_TAG
-echo "FROM $DOCKER_TAG" >> Dockerfile
+docker tag $DOCKER_TAG ilyaorson/symbolic_regression
+docker push ilyaorson/symbolic_regression
+echo "FROM $DOCKER_TAG" >> Dockerfile # this will always be one commit behind...
